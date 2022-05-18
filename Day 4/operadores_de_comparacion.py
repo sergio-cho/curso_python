513c5de86a5f65e74a23957c1d0d5bb55593140e
Sergio <sergiochogamez28@hotmail.com>
2022-05-18 12:49:06 -0500

    clase de comparadores

---
+++ Day 4/operadores_de_comparacion.py
@@ -0,0 +1,16 @@
+mi_bool = 10 == 25 #false
+
+print(mi_bool)
+
+'''Comparacion de strings'''
+
+mi_bool = "blanco" == "negro" 
+
+print(mi_bool) 
+
+'''Comparacion de int y float '''
+
+mi_bool = 100 == 100.0 
+
+print(mi_bool) 
+
